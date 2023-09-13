3948635a7ce33626b7af0b37614f266cef7b1992
sunseehray <sabtirazona@byui.edu>
2023-09-13 21:35:34 +0800

    Changed display for home URL to name

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello World');});
+router.get('/', (req, res) => {res.send('Sunseehray Tirazona');});
 
 router.use('/users', require('./users'));
 
