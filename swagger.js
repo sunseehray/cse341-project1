5855687b6d1500a1da17bfe3b49b3586735f58ed
sunseehray <sabtirazona@byui.edu>
2023-09-20 19:19:57 +0800

    corrected host from swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Users API',
         description: 'Users API'
     },
-    host: 'sunseehray341project1.onrender.com/',
+    host: 'sunseehray341project1.onrender.com',
     schemes: ['https']
 };
 
