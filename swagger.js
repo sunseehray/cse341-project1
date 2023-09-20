7d652485f03f762917161eefe1568679aa4488f4
sunseehray <sabtirazona@byui.edu>
2023-09-20 19:08:59 +0800

    changed host to render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'Users API',
         description: 'Users API'
     },
-    host: 'localhost:3000',
-    schemes: ['http', 'https']
+    host: 'sunseehray341project1.onrender.com/',
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
