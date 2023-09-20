027d21b9b3ff93b0a304f5ea7fca7713397b02f3
sunseehray <sabtirazona@byui.edu>
2023-09-20 18:56:20 +0800

    swagger update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Sunseehray Tirazona');});
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+    //#swagger.tags=['Sunseehray Tirazona']
+    res.send('Sunseehray Tirazona');
+});
 
 router.use('/users', require('./users'));
 
